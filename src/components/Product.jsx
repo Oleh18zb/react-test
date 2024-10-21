bf19e99d26a8ac17a6c054daac911df38fcbf416
Oleh18zb <110353014+Oleh18zb@users.noreply.github.com>
2024-10-21 20:29:58 +0200

    закінчив перший урок

---
+++ src/components/Product.jsx
@@ -0,0 +1,17 @@
+import PropTypes from 'prop-types';
+
+export default function Product({ name, imgUrl, price }) {
+    return (
+      <div>
+        <h2>{name}</h2>
+      <img src={imgUrl} alt={name} width="480" />
+      <p>Price: {price} credits</p>
+      </div>
+    );
+  };
+  
+  Product.propTypes = {
+    name: PropTypes.string.isRequired,
+    imgUrl: PropTypes.string,
+    price: PropTypes.number.isRequired,
+  };
